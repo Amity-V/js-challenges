82fcfeaf49b854b926aed67401a1141f3bcd3e72
xDamneDx <xdamnedx@gmail.com>
2022-11-25 10:12:03 +0200

    implement concatenateStrings

---
+++ RSSchool/core-js-101/01-strings-tasks.js
@@ -0,0 +1,16 @@
+/**
+ * Returns the result of concatenation of two strings.
+ *
+ * @param {string} value1
+ * @param {string} value2
+ * @return {string}
+ *
+ * @example
+ *   'aa', 'bb' => 'aabb'
+ *   'aa',''    => 'aa'
+ *   '',  'bb'  => 'bb'
+ */
+
+function concatenateStrings(value1, value2) {
+  return value1 + value2;
+}
