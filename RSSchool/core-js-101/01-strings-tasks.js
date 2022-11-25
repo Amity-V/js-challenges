e41b48f7de6d5dca76bc61aebbbd6482771321ac
xDamneDx <xdamnedx@gmail.com>
2022-11-25 10:14:34 +0200

    implement getStringLength

--- RSSchool/core-js-101/01-strings-tasks.js
+++ RSSchool/core-js-101/01-strings-tasks.js
@@ -14,3 +14,18 @@
 function concatenateStrings(value1, value2) {
   return value1 + value2;
 }
+
+/**
+ * Returns the length of given string.
+ *
+ * @param {string} value
+ * @return {number}
+ *
+ * @example
+ *   'aaaaa' => 5
+ *   'b'     => 1
+ *   ''      => 0
+ */
+function getStringLength(value) {
+  return value.length;
+}
