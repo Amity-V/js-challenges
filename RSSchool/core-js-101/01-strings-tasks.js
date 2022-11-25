83c62e1fa15f494b1287d63a10719dbb64dac80f
xDamneDx <xdamnedx@gmail.com>
2022-11-25 13:26:59 +0200

    implement removeFirstOccurrences

--- RSSchool/core-js-101/01-strings-tasks.js
+++ RSSchool/core-js-101/01-strings-tasks.js
@@ -114,3 +114,21 @@ function repeatString(value, count) {
   }
   return result;
 }
+
+/**
+ * Remove the first occurrence of string inside another string
+ *
+ * @param {string} str
+ * @param {string} value
+ * @return {string}
+ *
+ * @example
+ *   'To be or not to be', 'not'  => 'To be or  to be'
+ *   'I like legends', 'end' => 'I like legs',
+ *   'ABABAB','BA' => 'ABAB'
+ */
+
+function removeFirstOccurrences(str, value) {
+  const index = str.indexOf(value);
+  return str.slice(0, index) + str.slice(index + value.length);
+}
