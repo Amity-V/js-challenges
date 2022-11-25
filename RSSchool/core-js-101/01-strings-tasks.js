26cc3d281dc17bb6262e0543979f3ecb275eeafe
xDamneDx <xdamnedx@gmail.com>
2022-11-25 10:20:16 +0200

    implement getStringFromTemplate

--- RSSchool/core-js-101/01-strings-tasks.js
+++ RSSchool/core-js-101/01-strings-tasks.js
@@ -26,6 +26,25 @@ function concatenateStrings(value1, value2) {
  *   'b'     => 1
  *   ''      => 0
  */
+
 function getStringLength(value) {
   return value.length;
+}
+
+/**
+ * Returns the result of string template and given parameters firstName and lastName.
+ * Please do not use concatenation, use template string :
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/template_strings
+ *
+ * @param {string} firstName
+ * @param {string} lastName
+ * @return {string}
+ *
+ * @example
+ *   'John','Doe'      => 'Hello, John Doe!'
+ *   'Chuck','Norris'  => 'Hello, Chuck Norris!'
+ */
+
+function getStringFromTemplate(firstName, lastName) {
+  return `Hello, ${firstName} ${lastName}!`;
 }
