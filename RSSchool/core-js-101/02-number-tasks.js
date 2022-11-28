aa77f60936d60dbe9011857a29a97c64a00b4437
xDamneDx <xdamnedx@gmail.com>
2022-11-28 11:32:30 +0200

    implement getCircleCircumference

--- RSSchool/core-js-101/02-number-tasks.js
+++ RSSchool/core-js-101/02-number-tasks.js
@@ -13,3 +13,19 @@
 function getRectangleArea(width, height) {
   return width * height;
 }
+
+/**
+ * Returns a circumference of circle given by radius.
+ *
+ * @param {number} radius
+ * @return {number}
+ *
+ * @example:
+ *   5    => 31.41592653589793
+ *   3.14 => 19.729201864543903
+ *   0    => 0
+ */
+
+function getCircleCircumference(radius) {
+  return radius * (Math.PI * 2);
+}
