8f5d353259a8e1df924939c37108bd19fe4f05bc
xDamneDx <xdamnedx@gmail.com>
2022-11-28 11:47:00 +0200

    implement parseNumberFromString

--- RSSchool/core-js-101/02-number-tasks.js
+++ RSSchool/core-js-101/02-number-tasks.js
@@ -130,3 +130,19 @@ function getLastDigit(value) {
   const stringifiedNumber = String(value);
   return Number(stringifiedNumber[stringifiedNumber.length - 1]);
 }
+
+/**
+ * Returns a number by given string representation.
+ *
+ * @param {string} value
+ * @return {number}
+ *
+ * @example:
+ *    '100'     => 100
+ *     '37'     => 37
+ * '-525.5'     => -525.5
+ */
+
+function parseNumberFromString(value) {
+  return Number(value);
+}
