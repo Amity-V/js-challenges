767d168ecb29fa6beed41832540f4766594700ad
xDamneDx <xdamnedx@gmail.com>
2022-11-28 11:38:06 +0200

    implement getLinearEquationRoot

--- RSSchool/core-js-101/02-number-tasks.js
+++ RSSchool/core-js-101/02-number-tasks.js
@@ -68,3 +68,20 @@ function getDistanceBetweenPoints(x1, y1, x2, y2) {
   const b = Math.abs(y1) + Math.abs(y2);
   return Math.sqrt(a * a + b * b);
 }
+
+/**
+ * Returns a root of linear equation a*x + b = 0 given by coefficients a and b.
+ *
+ * @param {number} a
+ * @param {number} b
+ * @return {number}
+ *
+ * @example:
+ *   5*x - 10 = 0    => 2
+ *   x + 8 = 0       => -8
+ *   5*x = 0         => 0
+ */
+
+function getLinearEquationRoot(a, b) {
+  return -b / a;
+}
