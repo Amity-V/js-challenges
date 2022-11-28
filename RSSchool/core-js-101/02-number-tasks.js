36b0ddf5079a623e49ebb339b924fd655e74d59c
xDamneDx <xdamnedx@gmail.com>
2022-11-28 11:41:38 +0200

    implement getAngleBetweenVectors

--- RSSchool/core-js-101/02-number-tasks.js
+++ RSSchool/core-js-101/02-number-tasks.js
@@ -85,3 +85,30 @@ function getDistanceBetweenPoints(x1, y1, x2, y2) {
 function getLinearEquationRoot(a, b) {
   return -b / a;
 }
+
+/**
+ * Returns an angle (in radians) between two vectors given by xi and yi,
+ * coordinates in Cartesian plane.
+ * See details https://en.wikipedia.org/wiki/Euclidean_vector#Representations
+ *
+ * @param {number} x1
+ * @param {number} y1
+ * @param {number} x2
+ * @param {number} y2
+ * @return {number}
+ *
+ * @example:
+ *   (1,0) (0,1)     => π/2
+ *   (0,1) (0,-1)    => π
+ *   (0,-1) (1,0)    => π/2
+ *   (0,1) (0,1)     => 0
+ *   (0,1) (1,2)     => 0
+ */
+
+function getAngleBetweenVectors(x1, y1, x2, y2) {
+  const dot = x1 * x2 + y1 * y2;
+  const lenA = Math.sqrt(x1 * x1 + y1 * y1);
+  const lenB = Math.sqrt(x2 * x2 + y2 * y2);
+
+  return Math.acos(dot / (lenA * lenB));
+}
