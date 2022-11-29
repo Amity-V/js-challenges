7755cb9dd58b24a977b8e06050fe3cc758537db7
xDamneDx <xdamnedx@gmail.com>
2022-11-29 17:37:01 +0200

    implement removeFalsyValues

--- RSSchool/core-js-101/03-arrays-tasks.js
+++ RSSchool/core-js-101/03-arrays-tasks.js
@@ -85,3 +85,21 @@ function getArrayOfPositives(arr) {
 function getArrayOfStrings(arr) {
   return arr.filter((el) => typeof el === "string");
 }
+
+/**
+ * Removes falsy values from the specified array
+ * Falsy values: false, null, 0, "", undefined, and NaN.
+ * (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Boolean#Description)
+ *
+ * @param {array} arr
+ * @return {array}
+ *
+ * @example
+ *    [ 0, false, 'cat', NaN, true, '' ] => [ 'cat', true ]
+ *    [ 1, 2, 3, 4, 5, 'false' ]         => [ 1, 2, 3, 4, 5, 'false' ]
+ *    [ false, 0, NaN, '', undefined ]   => [ ]
+ */
+
+function removeFalsyValues(arr) {
+  return arr.filter((el) => !!el);
+}
