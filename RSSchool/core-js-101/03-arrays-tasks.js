f965f503c7e7ad980e745e130ba1d97c6110e439
xDamneDx <xdamnedx@gmail.com>
2022-11-29 17:05:18 +0200

    implement getArrayOfStrings

--- RSSchool/core-js-101/03-arrays-tasks.js
+++ RSSchool/core-js-101/03-arrays-tasks.js
@@ -69,3 +69,19 @@ function doubleArray(arr) {
 function getArrayOfPositives(arr) {
   return arr.filter((el) => el > 0);
 }
+
+/**
+ * Returns the array with strings only in the specified array (in original order)
+ *
+ * @param {array} arr
+ * @return {array}
+ *
+ * @example
+ *    [ 0, 1, 'cat', 3, true, 'dog' ] => [ 'cat', 'dog' ]
+ *    [ 1, 2, 3, 4, 5 ] => []
+ *    [ 'cat, 'dog', 'raccoon' ] => [ 'cat', 'dog', 'raccoon' ]
+ */
+
+function getArrayOfStrings(arr) {
+  return arr.filter((el) => typeof el === "string");
+}
