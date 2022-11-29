f8523e2fb2219cf02598165432f955bf58460283
xDamneDx <xdamnedx@gmail.com>
2022-11-29 16:59:49 +0200

    implement doubleArray

--- RSSchool/core-js-101/03-arrays-tasks.js
+++ RSSchool/core-js-101/03-arrays-tasks.js
@@ -36,3 +36,20 @@ function generateOdds(len) {
       return num;
     });
 }
+
+/**
+ * Returns the doubled array - elements of the specified array
+ * are repeated twice using original order
+ *
+ * @param {array} arr
+ * @return {array}
+ *
+ * @example
+ *    ['Ace', 10, true]  => ['Ace', 10, true,   'Ace', 10, true]
+ *    [0, 1, 2, 3, 4, 5] => [0, 1, 2, 3, 4, 5,   0, 1, 2, 3, 4, 5]
+ *    [] => []
+ */
+
+function doubleArray(arr) {
+  return [...arr, ...arr];
+}
