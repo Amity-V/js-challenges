ce670813bbf12a16fcc0cd0de7287bc50a13680c
xDamneDx <xdamnedx@gmail.com>
2022-11-29 17:03:26 +0200

    implement getArrayOfPositives

--- RSSchool/core-js-101/03-arrays-tasks.js
+++ RSSchool/core-js-101/03-arrays-tasks.js
@@ -53,3 +53,19 @@ function generateOdds(len) {
 function doubleArray(arr) {
   return [...arr, ...arr];
 }
+
+/**
+ * Returns an array of positive numbers from the specified array in original order
+ *
+ * @param {array} arr
+ * @return {array}
+ *
+ * @example
+ *    [ 0, 1, 2, 3, 4, 5 ] => [ 1, 2, 3, 4, 5 ]
+ *    [-1, 2, -5, -4, 0] => [ 2 ]
+ *    [] => []
+ */
+
+function getArrayOfPositives(arr) {
+  return arr.filter((el) => el > 0);
+}
