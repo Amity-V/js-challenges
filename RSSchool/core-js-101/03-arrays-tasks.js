827ca896e74c6b957cbe7d0da0aa01d5e73f0fc7
xDamneDx <xdamnedx@gmail.com>
2022-11-29 16:56:05 +0200

    implement generateOdds

--- RSSchool/core-js-101/03-arrays-tasks.js
+++ RSSchool/core-js-101/03-arrays-tasks.js
@@ -14,3 +14,25 @@
 function findElement(arr, value) {
   return arr.indexOf(value);
 }
+
+/**
+ * Generates an array of odd numbers of the specified length
+ *
+ * @param {number} len
+ * @return {array}
+ *
+ * @example
+ *    1 => [ 1 ]
+ *    2 => [ 1, 3 ]
+ *    5 => [ 1, 3, 5, 7, 9 ]
+ */
+
+function generateOdds(len) {
+  let num = -1;
+  return Array(len)
+    .fill(null)
+    .map(() => {
+      num += 2;
+      return num;
+    });
+}
