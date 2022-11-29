b1b72677a2fbfd2ba08e5b7c0195a4a2257f1fc5
xDamneDx <xdamnedx@gmail.com>
2022-11-29 17:47:17 +0200

    implement insertItem

--- RSSchool/core-js-101/03-arrays-tasks.js
+++ RSSchool/core-js-101/03-arrays-tasks.js
@@ -134,3 +134,19 @@ function getUpperCaseStrings(arr) {
 function getStringsLength(arr) {
   return arr.map((el) => el.length);
 }
+
+/**
+ * Inserts the item into specified array at specified index
+ *
+ * @param {array} arr
+ * @param {any} item
+ * @param {number} index
+ *
+ * @example
+ *    [ 1, 3, 4, 5 ], 2, 1  => [ 1, 2, 3, 4, 5 ]
+ *    [ 1, 'b', 'c'], 'x', 0  => [ 'x', 1, 'b', 'c' ]
+ */
+
+function insertItem(arr, item, index) {
+  return arr.splice(index, 0, item);
+}
