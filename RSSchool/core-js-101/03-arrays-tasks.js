4e4f02e7d9ced0d5b7562e13fd69fc8f3d0ff687
xDamneDx <xdamnedx@gmail.com>
2022-11-30 19:02:15 +0200

    implement getTail

--- RSSchool/core-js-101/03-arrays-tasks.js
+++ RSSchool/core-js-101/03-arrays-tasks.js
@@ -161,6 +161,22 @@ function insertItem(arr, item, index) {
  *    [ 1, 3, 4, 5 ], 2 => [ 1, 3 ]
  *    [ 'a', 'b', 'c', 'd'], 3  => [ 'a', 'b', 'c' ]
  */
+
 function getHead(arr, n) {
   return arr.splice(0, n);
+}
+
+/**
+ * Returns the n last items of the specified array
+ *
+ * @param {array} arr
+ * @param {number} n
+ *
+ * @example
+ *    [ 1, 3, 4, 5 ], 2  => [ 4, 5 ]
+ *    [ 'a', 'b', 'c', 'd'], 3  => [ 'b', 'c', 'd' ]
+ */
+
+function getTail(arr, n) {
+  return arr.splice(arr.length - n);
 }
