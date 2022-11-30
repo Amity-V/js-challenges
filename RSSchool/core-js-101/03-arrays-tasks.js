95b59d2d7d7fb10e65366133de457a7077ed2d50
xDamneDx <xdamnedx@gmail.com>
2022-11-30 19:03:33 +0200

    implement toCsvText

--- RSSchool/core-js-101/03-arrays-tasks.js
+++ RSSchool/core-js-101/03-arrays-tasks.js
@@ -180,3 +180,33 @@ function getHead(arr, n) {
 function getTail(arr, n) {
   return arr.splice(arr.length - n);
 }
+
+/**
+ * Returns CSV representation of two-dimensional numeric array.
+ * https://en.wikipedia.org/wiki/Comma-separated_values
+ *
+ * @param {array} arr
+ * @return {string}
+ *
+ * @example
+ *    [
+ *       [  0, 1, 2, 3, 4 ],
+ *       [ 10,11,12,13,14 ],
+ *       [ 20,21,22,23,24 ],
+ *       [ 30,31,32,33,34 ]
+ *    ]
+ *           =>
+ *     '0,1,2,3,4\n'
+ *    +'10,11,12,13,14\n'
+ *    +'20,21,22,23,24\n'
+ *    +'30,31,32,33,34'
+ */
+
+function toCsvText(arr) {
+  let str = "";
+  arr.map((el, index) => {
+    str += `${el.join()}${index === arr.length - 1 ? "" : "\n"}`;
+    return null;
+  });
+  return str;
+}
