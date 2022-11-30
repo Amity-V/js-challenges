e34a13feb15a37e2baeecfe6d739024b556d7b0c
xDamneDx <xdamnedx@gmail.com>
2022-11-30 18:59:44 +0200

    implement getHead

--- RSSchool/core-js-101/03-arrays-tasks.js
+++ RSSchool/core-js-101/03-arrays-tasks.js
@@ -150,3 +150,17 @@ function getStringsLength(arr) {
 function insertItem(arr, item, index) {
   return arr.splice(index, 0, item);
 }
+
+/**
+ * Returns the n first items of the specified array
+ *
+ * @param {array} arr
+ * @param {number} n
+ *
+ * @example
+ *    [ 1, 3, 4, 5 ], 2 => [ 1, 3 ]
+ *    [ 'a', 'b', 'c', 'd'], 3  => [ 'a', 'b', 'c' ]
+ */
+function getHead(arr, n) {
+  return arr.splice(0, n);
+}
