56fd0fd0b992312c78f318a160f4adb0e949ab32
xDamneDx <xdamnedx@gmail.com>
2022-12-01 22:10:43 +0200

    implement propagateItemsByPositionIndex

--- RSSchool/core-js-101/03-arrays-tasks.js
+++ RSSchool/core-js-101/03-arrays-tasks.js
@@ -267,3 +267,28 @@ function getMovingSum(arr) {
 function getSecondItems(arr) {
   return arr.filter((_, index) => index % 2);
 }
+
+/**
+ * Propagates every item in sequence its position times
+ * Returns an array that consists of: one first item, two second items, three third items etc.
+ *
+ * @param {array} arr
+ * @return {array}
+ *
+ * @example :
+ *  [] => []
+ *  [ 1 ] => [ 1 ]
+ *  [ 'a', 'b' ] => [ 'a', 'b','b' ]
+ *  [ 'a', 'b', 'c', null ] => [ 'a', 'b','b', 'c','c','c',  null,null,null,null ]
+ *  [ 1,2,3,4,5 ] => [ 1, 2,2, 3,3,3, 4,4,4,4, 5,5,5,5,5 ]
+ */
+
+function propagateItemsByPositionIndex(arr) {
+  return arr
+    .map((el, index) =>
+      Array(index + 1)
+        .fill(null)
+        .map(() => el)
+    )
+    .flat();
+}
