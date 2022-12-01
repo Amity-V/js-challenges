32aa06a15d765e42d92ba6fa825e3b540984bfbc
xDamneDx <xdamnedx@gmail.com>
2022-12-01 22:09:34 +0200

    implement getSecondItem

--- RSSchool/core-js-101/03-arrays-tasks.js
+++ RSSchool/core-js-101/03-arrays-tasks.js
@@ -251,3 +251,19 @@ function getMovingSum(arr) {
     return curr;
   });
 }
+
+/**
+ * Returns every second item from the specified array:
+ *
+ * @param {array} arr
+ * @return {array}
+ *
+ * Example :
+ * [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ] => [ 2, 4, 6, 8, 10 ]
+ * [ 'a', 'b', 'c' , null ]  => [ "b", null ]
+ * [ "a" ] => []
+ */
+
+function getSecondItems(arr) {
+  return arr.filter((_, index) => index % 2);
+}
