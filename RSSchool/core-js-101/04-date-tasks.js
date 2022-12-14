d71f753d3473d9e2e962caeb539157c8cdfcd41a
xDamneDx <xdamnedx@gmail.com>
2022-12-14 12:45:16 +0200

    implement parseDataFromRfc2822

---
+++ RSSchool/core-js-101/04-date-tasks.js
@@ -0,0 +1,16 @@
+/**
+ * Parses a rfc2822 string date representation into date value
+ * For rfc2822 date specification refer to : http://tools.ietf.org/html/rfc2822#page-14
+ *
+ * @param {string} value
+ * @return {date}
+ *
+ * @example:
+ *    'December 17, 1995 03:24:00'    => Date()
+ *    'Tue, 26 Jan 2016 13:48:02 GMT' => Date()
+ *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
+ */
+
+function parseDataFromRfc2822(value) {
+  return new Date(value);
+}
