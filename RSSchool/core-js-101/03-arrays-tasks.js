542351681eeebba025622c736de53e63193a6183
xDamneDx <xdamnedx@gmail.com>
2022-12-14 12:39:29 +0200

    implement swapHeadAndTail

--- RSSchool/core-js-101/03-arrays-tasks.js
+++ RSSchool/core-js-101/03-arrays-tasks.js
@@ -670,3 +670,36 @@ function getElementByIndexes(arr, indexes) {
 
   return result;
 }
+
+/**
+ * Swaps the head and tail of the specified array:
+ * the head (first half) of array move to the end, the tail (last half) move to the start.
+ * The middle element (if exists) leave on the same position.
+ *
+ *
+ * @param {array} arr
+ * @return {array}
+ *
+ * @example
+ *   [ 1, 2, 3, 4, 5 ]   =>  [ 4, 5, 3, 1, 2 ]
+ *    \----/   \----/
+ *     head     tail
+ *
+ *   [ 1, 2 ]  => [ 2, 1 ]
+ *   [ 1, 2, 3, 4, 5, 6, 7, 8 ]   =>  [ 5, 6, 7, 8, 1, 2, 3, 4 ]
+ *
+ */
+
+function swapHeadAndTail(arr) {
+  if (arr.length < 2) {
+    return arr;
+  }
+
+  const middle = Math.floor(arr.length / 2);
+  const head = arr.slice(0, middle);
+  const tail = arr.length % 2 === 0 ? arr.slice(middle) : arr.slice(middle + 1);
+
+  return arr.length % 2 === 0
+    ? [...tail, ...head]
+    : [...tail, arr[middle], ...head];
+}
