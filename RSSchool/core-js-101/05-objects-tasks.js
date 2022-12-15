ebea7476202b4c4786ca3a68bc3c9292b25cba28
xDamneDx <xdamnedx@gmail.com>
2022-12-15 15:03:20 +0200

    Implement Rectangle

---
+++ RSSchool/core-js-101/05-objects-tasks.js
@@ -0,0 +1,23 @@
+/**
+ * Returns the rectangle object with width and height parameters and getArea() method
+ *
+ * @param {number} width
+ * @param {number} height
+ * @return {Object}
+ *
+ * @example
+ *    const r = new Rectangle(10,20);
+ *    console.log(r.width);       // => 10
+ *    console.log(r.height);      // => 20
+ *    console.log(r.getArea());   // => 200
+ */
+
+function Rectangle(width, height) {
+  return {
+    width,
+    height,
+    getArea() {
+      return width * height;
+    },
+  };
+}
