96d78fe59f1527a33fc2e5cb55aab963888e3cd6
xDamneDx <xdamnedx@gmail.com>
2022-12-15 14:58:52 +0200

    implement parseDataFromIso8601

--- RSSchool/core-js-101/04-date-tasks.js
+++ RSSchool/core-js-101/04-date-tasks.js
@@ -14,3 +14,19 @@
 function parseDataFromRfc2822(value) {
   return new Date(value);
 }
+
+/**
+ * Parses an ISO 8601 string date representation into date value
+ * For ISO 8601 date specification refer to : https://en.wikipedia.org/wiki/ISO_8601
+ *
+ * @param {string} value
+ * @return {date}
+ *
+ * @example :
+ *    '2016-01-19T16:07:37+00:00'    => Date()
+ *    '2016-01-19T08:07:37Z' => Date()
+ */
+
+function parseDataFromIso8601(value) {
+  return new Date(value);
+}
