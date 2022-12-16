437672783d987038e9f514e15b6c81fb00047102
xDamneDx <xdamnedx@gmail.com>
2022-12-16 17:05:03 +0200

    Implement fromJSON

--- RSSchool/core-js-101/05-objects-tasks.js
+++ RSSchool/core-js-101/05-objects-tasks.js
@@ -36,3 +36,21 @@ function Rectangle(width, height) {
 function getJSON(obj) {
   return JSON.stringify(obj);
 }
+
+/**
+ * Returns the object of specified type from JSON representation
+ *
+ * @param {Object} proto
+ * @param {string} json
+ * @return {object}
+ *
+ * @example
+ *    const r = fromJSON(Circle.prototype, '{"radius":10}');
+ *
+ */
+
+function fromJSON(proto, json) {
+  const values = Object.values(JSON.parse(json));
+
+  return new proto.constructor(...values);
+}
