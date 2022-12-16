1866b6fa948212a4148d60a75e3165d3a1c62b71
xDamneDx <xdamnedx@gmail.com>
2022-12-16 17:12:34 +0200

    Implement getFizzBuzz

---
+++ RSSchool/core-js-101/07-conditions-n-loops-tasks.js
@@ -0,0 +1,27 @@
+/**
+ * Returns the 'Fizz','Buzz' or an original number using the following rules:
+ * 1) return original number
+ * 2) but if number multiples of three return 'Fizz'
+ * 3) for the multiples of five return 'Buzz'
+ * 4) for numbers which are multiples of both three and five return 'FizzBuzz'
+ *
+ * @param {number} num
+ * @return {any}
+ *
+ * @example
+ *   2 =>  2
+ *   3 => 'Fizz'
+ *   5 => 'Buzz'
+ *   4 => 4
+ *  15 => 'FizzBuzz'
+ *  20 => 'Buzz'
+ *  21 => 'Fizz'
+ *
+ */
+
+function getFizzBuzz(num) {
+  if (!(num % 3) && !(num % 5)) return "FizzBuzz";
+  if (!(num % 5)) return "Buzz";
+  if (!(num % 3)) return "Fizz";
+  return num;
+}
