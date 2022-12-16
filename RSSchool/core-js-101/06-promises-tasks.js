2e571dd50b0646cdd2c22f6d9600beeab4d47bd6
xDamneDx <xdamnedx@gmail.com>
2022-12-16 17:07:23 +0200

    Implement processAllPromises

--- RSSchool/core-js-101/06-promises-tasks.js
+++ RSSchool/core-js-101/06-promises-tasks.js
@@ -32,3 +32,23 @@ function willYouMarryMe(isPositiveAnswer) {
     }
   });
 }
+
+/**
+ * Return Promise object that should be resolved with array containing plain values.
+ * Function receive an array of Promise objects.
+ *
+ * @param {Promise[]} array
+ * @return {Promise}
+ *
+ * @example
+ *    const promises = [Promise.resolve(1), Promise.resolve(3), Promise.resolve(12)]
+ *    const p = processAllPromises(promises);
+ *    p.then((res) => {
+ *      console.log(res) // => [1, 2, 3]
+ *    })
+ *
+ */
+
+function processAllPromises(array) {
+  return Promise.all(array);
+}
