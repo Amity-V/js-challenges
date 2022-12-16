d54f6f12cf18b06ab3d742d0e7ebbe1769ac7051
xDamneDx <xdamnedx@gmail.com>
2022-12-16 17:08:32 +0200

    Implement getFastestPromise

--- RSSchool/core-js-101/06-promises-tasks.js
+++ RSSchool/core-js-101/06-promises-tasks.js
@@ -52,3 +52,27 @@ function willYouMarryMe(isPositiveAnswer) {
 function processAllPromises(array) {
   return Promise.all(array);
 }
+
+/**
+ * Return Promise object that should be resolved with value received from
+ * Promise object that will be resolved first.
+ * Function receive an array of Promise objects.
+ *
+ * @param {Promise[]} array
+ * @return {Promise}
+ *
+ * @example
+ *    const promises = [
+ *      Promise.resolve('first'),
+ *      new Promise(resolve => setTimeout(() => resolve('second'), 500)),
+ *    ];
+ *    const p = processAllPromises(promises);
+ *    p.then((res) => {
+ *      console.log(res) // => [first]
+ *    })
+ *
+ */
+
+function getFastestPromise(array) {
+  return Promise.race(array);
+}
