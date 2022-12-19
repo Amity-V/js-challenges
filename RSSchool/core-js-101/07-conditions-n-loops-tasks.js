5658268466994e1ba3ac3254f2f919fe2da578dc
xDamneDx <xdamnedx@gmail.com>
2022-12-19 20:33:24 +0200

    Implement getDigitalRoot

--- RSSchool/core-js-101/07-conditions-n-loops-tasks.js
+++ RSSchool/core-js-101/07-conditions-n-loops-tasks.js
@@ -166,3 +166,33 @@ function reverseInteger(num) {
 
   return Number(reversedNums.join(""));
 }
+
+/**
+ * Returns the digital root of integer:
+ *   step1 : find sum of all digits
+ *   step2 : if sum > 9 then goto step1 otherwise return the sum
+ *
+ * @param {number} n
+ * @return {number}
+ *
+ * @example:
+ *   12345 ( 1+2+3+4+5 = 15, 1+5 = 6) => 6
+ *   23456 ( 2+3+4+5+6 = 20, 2+0 = 2) => 2
+ *   10000 ( 1+0+0+0+0 = 1 ) => 1
+ *   165536 (1+6+5+5+3+6 = 26,  2+6 = 8) => 8
+ */
+
+function getDigitalRoot(num) {
+  const sum = num
+    .toString()
+    .split("")
+    .reduce((prev, current) => Number(prev) + Number(current));
+  const result =
+    sum < 10
+      ? sum
+      : sum
+          .toString()
+          .split("")
+          .reduce((prev, current) => Number(prev) + Number(current));
+  return result;
+}
