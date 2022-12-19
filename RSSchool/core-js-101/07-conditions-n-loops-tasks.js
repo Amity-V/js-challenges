febc540e7a65f12869853d3e7f88b04552d31e05
xDamneDx <xdamnedx@gmail.com>
2022-12-19 20:21:00 +0200

    Implement getIntervalString

--- RSSchool/core-js-101/07-conditions-n-loops-tasks.js
+++ RSSchool/core-js-101/07-conditions-n-loops-tasks.js
@@ -87,3 +87,34 @@ function findFirstSingleChar(str) {
 
   return result;
 }
+
+/**
+ * Returns the string representation of math interval,
+ * specified by two points and include / exclude flags.
+ * See the details: https://en.wikipedia.org/wiki/Interval_(mathematics)
+ *
+ * Please take attention, that the smaller number should be the first in the notation
+ *
+ * @param {number} a
+ * @param {number} b
+ * @param {bool} isStartIncluded
+ * @param {bool} isEndIncluded
+ * @return {string}
+ *
+ * @example
+ *   0, 1, true, true   => '[0, 1]'
+ *   0, 1, true, false  => '[0, 1)'
+ *   0, 1, false, true  => '(0, 1]'
+ *   0, 1, false, false => '(0, 1)'
+ * Smaller number has to be first :
+ *   5, 3, true, true   => '[3, 5]'
+ *
+ */
+
+function getIntervalString(a, b, isStartIncluded, isEndIncluded) {
+  const start = isStartIncluded ? "[" : "(";
+  const end = isEndIncluded ? "]" : ")";
+  const nums = a < b ? `${a}, ${b}` : `${b}, ${a}`;
+
+  return start + nums + end;
+}
