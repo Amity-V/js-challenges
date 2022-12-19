4dee441011356029a324d7499b90e3da53630ce2
xDamneDx <xdamnedx@gmail.com>
2022-12-19 20:30:09 +0200

    Implement reverseString

--- RSSchool/core-js-101/07-conditions-n-loops-tasks.js
+++ RSSchool/core-js-101/07-conditions-n-loops-tasks.js
@@ -118,3 +118,27 @@ function getIntervalString(a, b, isStartIncluded, isEndIncluded) {
 
   return start + nums + end;
 }
+
+/**
+ * Reverse the specified string (put all chars in reverse order)
+ *
+ * @param {string} str
+ * @return {string}
+ *
+ * @example:
+ * 'The quick brown fox jumps over the lazy dog' => 'god yzal eht revo spmuj xof nworb kciuq ehT'
+ * 'abracadabra' => 'arbadacarba'
+ * 'rotator' => 'rotator'
+ * 'noon' => 'noon'
+ */
+
+function reverseString(str) {
+  const chars = str.split("");
+  const result = [];
+
+  chars.forEach((char) => {
+    result.unshift(char);
+  });
+
+  return result.join("");
+}
