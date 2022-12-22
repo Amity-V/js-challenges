14aca0454617c86b40866906597ea286ce2b850b
xDamneDx <xdamnedx@gmail.com>
2022-12-22 15:47:19 +0200

    Implement toNaryString

--- RSSchool/core-js-101/07-conditions-n-loops-tasks.js
+++ RSSchool/core-js-101/07-conditions-n-loops-tasks.js
@@ -462,3 +462,28 @@ function isCreditCardNumber(ccn) {
 
   return (10 - (result % 10)) % 10 === 0;
 }
+
+/**
+ * Returns the string with n-ary (binary, ternary, etc, where n <= 10)
+ * representation of specified number.
+ * See more about
+ * https://en.wikipedia.org/wiki/Binary_number
+ * https://en.wikipedia.org/wiki/Ternary_numeral_system
+ * https://en.wikipedia.org/wiki/Radix
+ *
+ * @param {number} num
+ * @param {number} n, radix of the result
+ * @return {string}
+ *
+ * @example:
+ *   1024, 2  => '10000000000'
+ *   6561, 3  => '100000000'
+ *    365, 2  => '101101101'
+ *    365, 3  => '111112'
+ *    365, 4  => '11231'
+ *    365, 10 => '365'
+ */
+
+function toNaryString(num, n) {
+  return num.toString(n);
+}
