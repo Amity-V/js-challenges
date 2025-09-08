db7d911f87b2ce50b0fd49f77be1c8d2d0ae295a
Amity <46899094+Amity-V@users.noreply.github.com>
2025-09-08 19:27:16 +0100

    add solution

---
+++ codecademy/fundametals/02-agreeOrDisagree.js
@@ -0,0 +1,11 @@
+// Write a function, agreeOrDisagree(), that takes in two strings,
+// and returns 'You agree!' if the two strings are the same and
+// 'You disagree!' if the two strings are different.
+
+// Write your function here:
+
+const agreeOrDisagree = (string1, string2) =>
+    string1 === string2 ? "You agree!" : "You disagree!";
+
+// Uncomment the line below when you're ready to try out your function
+console.log(agreeOrDisagree("yep", "yep")) // Should print 'You agree!'
