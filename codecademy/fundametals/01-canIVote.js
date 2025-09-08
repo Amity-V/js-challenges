50645f4acbc6f03c294d78070fdfe78669671dce
Amity <46899094+Amity-V@users.noreply.github.com>
2025-09-08 19:24:33 +0100

    add solution

---
+++ codecademy/fundametals/01-canIVote.js
@@ -0,0 +1,11 @@
+// The most common minimum age to vote is 18. Write a function canIVote()
+// that takes in a number, representing a person’s age, and returns the
+// boolean true if they are 18 years old or older, and the boolean false
+// if they are not.
+
+// Write your function here:
+
+const canIVote = (age) => age >= 18;
+
+// Uncomment the line below when you're ready to try out your function
+console.log(canIVote(19)) // Should print true
