02eef6a809f769f7d998393048e87fd80f04a4dc
Amity <46899094+Amity-V@users.noreply.github.com>
2025-09-10 17:10:16 +0100

    add solution

---
+++ codecademy/fundametals/05-reportingForDuty.js
@@ -0,0 +1,14 @@
+// Write a function, reportingForDuty(), that has two string parameters,
+// rank and lastName, and returns a string in the following format:
+// ‘rank lastName reporting for duty!’
+
+// reportingForDuty('Private', 'Fido') 
+// Should return 'Private Fido reporting for duty!'
+
+// Write your function here:
+
+const reportingForDuty = (rank, lastName) =>
+    `${rank} ${lastName} reporting for duty!`;
+
+// Uncomment the line below when you're ready to try out your function
+console.log(reportingForDuty('Private', 'Fido')) // Should return 'Private Fido reporting for duty!'
