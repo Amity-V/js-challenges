d9d7ed5dc9c4562c957b798dc97f37390468a372
Amity <46899094+Amity-V@users.noreply.github.com>
2025-09-12 20:36:32 +0100

    add solution

---
+++ codecademy/fundametals/07-calculateWeight.js
@@ -0,0 +1,44 @@
+// Though an object’s mass remains consistent throughout the universe, 
+// weight is determined by the force of gravity on an object. Since 
+// different planets have different gravity, the same object would weigh 
+// different amounts on each of those planets! Cool, huh?
+
+// Write a function, calculateWeight(). It should:
+
+// - take two parameters: earthWeight and planet
+// - expect earthWeight to be a number
+// - expect planet to be a string
+// - return a number representing what that Earth-weight would equate to on the planet passed in.
+
+// The final weight should be calculated based on the following:
+
+// - 'Mercury' weight = earthWeight * 0.378
+// - 'Venus' weight = earthWeight * 0.907
+// - 'Mars' weight = earthWeight * 0.377<
+// - 'Jupiter' weight = earthWeight * 2.36
+// - 'Saturn' weight = earthWeight * 0.916
+
+// For any other planetary inputs, return 'Invalid Planet Entry. Try: Mercury,
+// Venus, Mars, Jupiter, or Saturn.'
+
+// Write your function here:
+
+const calculateWeight = (earthWeight, planet) => {
+    switch (planet) {
+        case "Mercury":
+            return earthWeight * 0.378;
+        case "Venus":
+            return earthWeight * 0.907;
+        case "Mars":
+            return earthWeight * 0.377;
+        case "Jupiter":
+            return earthWeight * 2.36;
+        case "Saturn":
+            return earthWeight * 0.916;
+        default:
+            return "Invalid Planet Entry. Try: Mercury, Venus, Mars, Jupiter, or Saturn.";
+    }
+};
+
+// Uncomment the line below when you're ready to try out your function
+console.log(calculateWeight(100, "Jupiter")); // Should print 236
