1193304de707de6fcdb276656974ac21b34df947
Amity <46899094+Amity-V@users.noreply.github.com>
2025-10-13 17:58:36 +0100

    add objective & tests

---
+++ freecodecamp/03_functions/build_a_boolean_check_function.js
@@ -0,0 +1,27 @@
+// In this lab you will build a function that check if a value is classified as a boolean primitive.
+
+// Boolean primitives are true and false.
+
+// Objective: Fulfill the user stories below and get all the tests to pass to complete the lab.
+
+// User Stories:
+
+// You should have a function called booWho that receives one argument.
+// If the argument received is a boolean primitive, the function should return true.
+// If the argument is any other value, the function should return false.
+
+
+
+// Tests
+
+// 1. You should have a booWho function.
+// 2. booWho(true) should return true.
+// 3. booWho(false) should return true.
+// 4. booWho([1, 2, 3]) should return false.
+// 5. booWho([].slice) should return false.
+// 6. booWho({ "a": 1 }) should return false.
+// 7. booWho(1) should return false.
+// 8. booWho(NaN) should return false.
+// 9. booWho("a") should return false.
+// 10. booWho("true") should return false.
+// 11. booWho("false") should return false.
