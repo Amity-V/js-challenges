fb8d6e350c3314a414284b52897a6f060b74eba3
Amity <46899094+Amity-V@users.noreply.github.com>
2025-10-15 18:28:50 +0100

    add objective & tests

---
+++ freecodecamp/03_functions/celsius_to_fahrenheit_converter.js
@@ -0,0 +1,24 @@
+// In this lab, you will write a function that converts the temperature from Celsius to Fahrenheit. The formula to convert from Celsius to Fahrenheit is:
+
+// fahrenheit = celsius * (9/5) + 32
+
+// Objective: Fulfill the user stories below and get all the tests to pass to complete the lab.
+
+// User Stories:
+
+// You should create a function named convertCtoF.
+// convertCtoF should take a single numeric argument, which is the temperature in Celsius.
+// convertCtoF should return a number.
+
+
+
+// Tests
+
+// 1. You should create a function named convertCtoF.
+// 2. convertCtoF should take a single parameter.
+// 3. convertCtoF(0) should return a number.
+// 4. convertCtoF(-30) should return a value of -22.
+// 5. convertCtoF(-10) should return a value of 14.
+// 6. convertCtoF(0) should return a value of 32.
+// 7. convertCtoF(20) should return a value of 68.
+// 8. convertCtoF(30) should return a value of 86.
