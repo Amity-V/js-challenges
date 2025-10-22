bef2774f4ff7598746b6617e152ec93eb9aa33a1
Amity <46899094+Amity-V@users.noreply.github.com>
2025-10-22 17:08:19 +0100

    add solution

--- freecodecamp/03_functions/confirm_the_ending_tool.js
+++ freecodecamp/03_functions/confirm_the_ending_tool.js
@@ -8,7 +8,7 @@
 // 2. The function should return true if the first string ends with the second string, and false otherwise.
 // 3. You should not use the .endsWith() method; instead, use one of the JavaScript substring methods to achieve this.
 
-
+const confirmEnding = (str, target) => str.slice(-target.length) === target;
 
 // Tests
 
