d90c1fd54d90a8d0b5deda8f8f1d018ccc18565d
Amity <46899094+Amity-V@users.noreply.github.com>
2025-11-03 16:48:17 +0000

    add solution

--- freecodecamp/05_objects/record_collection.js
+++ freecodecamp/05_objects/record_collection.js
@@ -18,7 +18,41 @@
 // 4. If prop is tracks and value isn't an empty string, add value to the end of the album's existing tracks array.
 // Note: A copy of the recordCollection object is used for the tests. Your function should not directly refer to the recordCollection object, only the function parameter.
 
+const recordCollection = {
+    2548: {
+        albumTitle: 'Slippery When Wet',
+        artist: 'Bon Jovi',
+        tracks: ['Let It Rock', 'You Give Love a Bad Name']
+    },
+    2468: {
+        albumTitle: '1999',
+        artist: 'Prince',
+        tracks: ['1999', 'Little Red Corvette']
+    },
+    1245: {
+        artist: 'Robert Palmer',
+        tracks: []
+    },
+    5439: {
+        albumTitle: 'ABBA Gold'
+    }
+};
 
+const updateRecords = (records, id, prop, value) => {
+    if (value === "") {
+        delete records[id][prop];
+    } else if (prop !== "tracks") {
+        records[id][prop] = value;
+    } else {
+        if (!records[id].tracks) {
+            records[id].tracks = [];
+        }
+
+        records[id].tracks.push(value);
+    }
+
+    return records;
+}
 
 // Tests
 
