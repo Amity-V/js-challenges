6b608846d27c887e7d68e9bd19e910a5986cd692
Amity <46899094+Amity-V@users.noreply.github.com>
2025-11-11 16:26:54 +0000

    add solution

--- freecodecamp/06_loops/chunky_monkey_algorithm.js
+++ freecodecamp/06_loops/chunky_monkey_algorithm.js
@@ -4,7 +4,15 @@
 
 // Write a function named chunkArrayInGroups that takes an array as first argument and a number as second argument. The function should split the array into smaller arrays of length equal to the second argument and returns them as a two-dimensional array.
 
+const chunkArrayInGroups = (arr, size) => {
+    const result = [];
 
+    for (let i = 0; i < arr.length; i += size) {
+        result.push(arr.slice(i, i + size));
+    }
+
+    return result;
+}
 
 // Tests
 
