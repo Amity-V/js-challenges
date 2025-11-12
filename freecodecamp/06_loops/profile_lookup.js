06e3f22f07b8b51b0634314778649060c42c8602
Amity <46899094+Amity-V@users.noreply.github.com>
2025-11-12 16:18:23 +0000

    add objective & tests

---
+++ freecodecamp/06_loops/profile_lookup.js
@@ -0,0 +1,27 @@
+// In this lab, you will build a profile lookup that looks up information about people in a contacts list.
+
+// For this example imagine there is a contact named Akira Laine, the lookUpProfile("Akira", "lastName") should return Laine.
+
+// Objective: Fulfill the user stories below and get all the tests to pass to complete the lab.
+
+// User Stories:
+
+// 1. You should create a function named lookUpProfile that takes a name and a property as arguments.
+// 2. You should retrieve contact information from the provided contacts array.
+// 3. If the function receives a contact name and the property exists on the related contact, then the property's value should be returned.
+// 4. If the name passed to the function does not match any contacts in the contacts array, then the function should return "No such contact".
+// 5. If the property does not exist on a found contact, then the function should return "No such property".
+
+
+
+// Tests
+
+// 1. The contacts array should still be present in the global scope. Reset the lesson to recover it if you deleted it.
+// 2. You should have a function named lookUpProfile.
+// 3. lookUpProfile("Kristian", "lastName") should return a string.
+// 4. lookUpProfile("Kristian", "lastName") should return the string Vos
+// 5. lookUpProfile("Sherlock", "likes") should return ["Intriguing Cases", "Violin"]
+// 6. lookUpProfile("Harry", "likes") should return an array
+// 7. lookUpProfile("Bob", "number") should return the string No such contact
+// 8. lookUpProfile("Bob", "potato") should return the string No such contact
+// 9. lookUpProfile("Akira", "address") should return the string No such property
