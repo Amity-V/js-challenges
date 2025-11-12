c4953aa5cc1b48ae821b9b26451519a251597e78
Amity <46899094+Amity-V@users.noreply.github.com>
2025-11-12 16:26:50 +0000

    add solution

--- freecodecamp/06_loops/profile_lookup.js
+++ freecodecamp/06_loops/profile_lookup.js
@@ -12,7 +12,43 @@
 // 4. If the name passed to the function does not match any contacts in the contacts array, then the function should return "No such contact".
 // 5. If the property does not exist on a found contact, then the function should return "No such property".
 
+let contacts = [
+    {
+        firstName: "Akira",
+        lastName: "Laine",
+        number: "0543236543",
+        likes: ["Pizza", "Coding", "Brownie Points"],
+    },
+    {
+        firstName: "Harry",
+        lastName: "Potter",
+        number: "0994372684",
+        likes: ["Hogwarts", "Magic", "Hagrid"],
+    },
+    {
+        firstName: "Sherlock",
+        lastName: "Holmes",
+        number: "0487345643",
+        likes: ["Intriguing Cases", "Violin"],
+    },
+    {
+        firstName: "Kristian",
+        lastName: "Vos",
+        number: "unknown",
+        likes: ["JavaScript", "Gaming", "Foxes"],
+    },
+];
 
+const lookUpProfile = (name, property) => {
+    for (let i = 0; i < contacts.length; i++) {
+        if (contacts[i].firstName === name) {
+            return property in contacts[i]
+                ? contacts[i][property]
+                : "No such property"
+        }
+    }
+    return "No such contact";
+}
 
 // Tests
 
