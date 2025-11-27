b2dd30b13b99510feee99d05e69433b4e59da6c3
Amity <46899094+Amity-V@users.noreply.github.com>
2025-11-27 18:38:15 +0000

    add objective & tests

---
+++ freecodecamp/07_js_fundamentals/title_case_converter.js
@@ -0,0 +1,23 @@
+// In this lab you will create a function that converts a string to title case. Title case means that the first letter of each word is capitalized and the rest of the word is in lower case.
+
+// "Web Development Is Awesome" is an example of a title cased string.
+
+// Objective: Fulfill the user stories below and get all the tests to pass to complete the lab.
+
+// User Stories:
+
+// 1. You should have a titleCase function that takes a string as an argument.
+// 2. The titleCase function should return a string with the first letter of each word capitalized and the rest of the word in lower case.
+// 3. titleCase("I like to code") should return "I Like To Code".
+// 4. titleCase("javaScript is fun") should return "Javascript Is Fun".
+
+
+
+// Tests
+
+// 1. You should create a function named titleCase.
+// 2. titleCase should take a single argument.
+// 3. titleCase("I'm a little tea pot") should return a string.
+// 4. titleCase("I'm a little tea pot") should return the string I'm A Little Tea Pot.
+// 5. titleCase("sHoRt AnD sToUt") should return the string Short And Stout.
+// 6. titleCase("HERE IS MY HANDLE HERE IS MY SPOUT") should return the string Here Is My Handle Here Is My Spout.
