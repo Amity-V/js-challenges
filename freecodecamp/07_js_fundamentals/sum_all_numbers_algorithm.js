98cfb326bd534c5f7f9a41f730d5631f16eac1f5
Amity <46899094+Amity-V@users.noreply.github.com>
2025-12-09 23:14:51 +0000

    add objective & tests

---
+++ freecodecamp/07_js_fundamentals/sum_all_numbers_algorithm.js
@@ -0,0 +1,17 @@
+// In this lab, you will need to design a sum all numbers algorithm.
+
+// Objective: Fulfill the user stories below and get all the tests to pass to complete the lab.
+
+// User Stories:
+
+// 1. You should have a function named sumAll that accepts an array of two numbers.
+// 2. sumAll([n, m]) should return the sum of n and m plus the sum of all the numbers between them. The lowest number will not always come first. For example, sumAll([4,1]) should return 10 because sum of all the numbers between 1 and 4 (both inclusive) is 10.
+
+
+
+// 1. You should have a function named sumAll.
+// 2. sumAll([1, 4]) should return a number.
+// 3. sumAll([1, 4]) should return 10.
+// 4. sumAll([4, 1]) should return 10.
+// 5. sumAll([5, 10]) should return 45.
+// 6. sumAll([10, 5]) should return 45.
