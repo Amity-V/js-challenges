18a765e2c918137417a568a5081ef36360e57913
Amity <46899094+Amity-V@users.noreply.github.com>
2025-12-12 21:12:58 +0000

    add solution

--- freecodecamp/07_js_fundamentals/dna_pair_generator.js
+++ freecodecamp/07_js_fundamentals/dna_pair_generator.js
@@ -17,7 +17,16 @@
 // 5. When given C, the function should pair it with G.
 // 6. When given G, the function should pair it with C.
 
+const pairElement = (str) => {
+    const pairs = {
+        A: "T",
+        T: "A",
+        C: "G",
+        G: "C"
+    };
 
+    return str.split("").map(base => [base, pairs[base]]);
+}
 
 // Tests
 
