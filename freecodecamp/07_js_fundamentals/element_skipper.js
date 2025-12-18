b1ae33f15a710cb0f7b6003cde2b07662e0561ee
Amity <46899094+Amity-V@users.noreply.github.com>
2025-12-18 17:05:54 +0000

    add solution

--- freecodecamp/07_js_fundamentals/element_skipper.js
+++ freecodecamp/07_js_fundamentals/element_skipper.js
@@ -11,7 +11,11 @@
 // 3. The dropElements function should return the remaining elements in the array if the condition is met.
 // 4. If the condition is never satisfied, it should return an empty array.
 
+const dropElements = (arr, func) => {
+    const index = arr.findIndex(func);
 
+    return index === -1 ? [] : arr.slice(index);
+}
 
 // Tests
 
