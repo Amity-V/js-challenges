85969f7f7d13dd4f4a3bc6d700f73a98b05224f4
Amity <46899094+Amity-V@users.noreply.github.com>
2025-12-29 17:39:43 +0000

    add objective & tests

---
+++ freecodecamp/08_higher_order_functions_and_callbacks/matching_object_filter.js
@@ -0,0 +1,32 @@
+// In this lab, you will create a function that filters an array of objects and returns only those objects that match all key-value pairs in a given source object.
+
+// Objective: Fulfill the user stories below and get all the tests to pass to complete the lab.
+
+// User Stories:
+
+// 1. You should have a whatIsInAName function that accepts two arguments, an array of objects and a source object.
+// 2. The whatIsInAName function should return a new array containing only the objects from the collection that have all the key–value pairs present in the source object.
+// 3. If no objects match all the key–value pairs from the source, the function should return an empty array. For example:
+
+// whatIsInAName(
+//   [
+//     { first: "Romeo", last: "Montague" },
+//     { first: "Mercutio", last: null },
+//     { first: "Tybalt", last: "Capulet" }
+//   ],
+//   { last: "Capulet" }
+// );
+// // should return [{ first: "Tybalt", last: "Capulet" }]
+
+
+
+// Tests
+
+// 1. You should have a whatIsInAName function.
+// 2. whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" }) should return [{ first: "Tybalt", last: "Capulet" }].
+// 3. whatIsInAName([{ "apple": 1 }, { "apple": 1 }, { "apple": 1, "bat": 2 }], { "apple": 1 }) should return [{"apple": 1}, {"apple": 1}, {"apple": 1, "bat": 2}].
+// 4. whatIsInAName([{ "apple": 1, "bat": 2 }, { "bat": 2 }, { "apple": 1, "bat": 2, "cookie": 2 }], { "apple": 1, "bat": 2 }) should return [{"apple": 1, "bat": 2}, {"apple": 1, "bat": 2, "cookie": 2}].
+// 5. whatIsInAName([{ "apple": 1, "bat": 2 }, { "apple": 1 }, { "apple": 1, "bat": 2, "cookie": 2 }], { "apple": 1, "cookie": 2 }) should return [{"apple": 1, "bat": 2, "cookie": 2}].
+// 6. whatIsInAName([{ "apple": 1, "bat": 2 }, { "apple": 1 }, { "apple": 1, "bat": 2, "cookie": 2 }, { "bat": 2 }], { "apple": 1, "bat": 2 }) should return [{"apple": 1, "bat": 2}, {"apple": 1, "bat": 2, "cookie": 2}]
+// 7. whatIsInAName([{"a": 1, "b": 2, "c": 3}], {"a": 1, "b": 9999, "c": 3}) should return [].
+// 8. whatIsInAName([{"a": 1, "b": 2, "c": 3, "d": 9999}], {"a": 1, "b": 9999, "c": 3}) should return [].
