09ee638b706688ea2cf09104c34910cf9064d21e
Amity <46899094+Amity-V@users.noreply.github.com>
2025-12-30 17:20:35 +0000

    add objective & tests

---
+++ freecodecamp/08_higher_order_functions_and_callbacks/prime_number_sum_calculator.js
@@ -0,0 +1,20 @@
+// In this lab, you will build a calculator that takes a number and returns the sum of all prime numbers that are less than or equal to that number.
+
+// Objective: Fulfill the user stories below and get all the tests to pass to complete the lab.
+
+// User Stories:
+
+// 1. You should have a sumPrimes function that accepts a number as an argument.
+// 2. The sumPrimes function should return the sum of all prime numbers less than or equal to the provided number.
+// 3. If the input number is less than 2, the function should return 0.
+
+
+
+// Tests
+
+// 1. You should have a sumPrimes function.
+// 2. sumPrimes(10) should return 17.
+// 3. sumPrimes(5) should return 10.
+// 4. sumPrimes(2) should return 2.
+// 5. sumPrimes(0) should return 0.
+// 6. sumPrimes(977) should return 73156.
