01c58957e5a3583930c3c78cb48ea522f57c3323
Amity <46899094+Amity-V@users.noreply.github.com>
2025-12-31 17:52:08 +0000

    add objective & tests

---
+++ freecodecamp/08_higher_order_functions_and_callbacks/range-based_lcm_calculator.js
@@ -0,0 +1,21 @@
+// In this lab, you will create a function that takes an array of two numbers and returns the least common multiple (LCM) of those two numbers and all the numbers between them.
+
+// Objective: Fulfill the user stories below and get all the tests to pass to complete the lab.
+
+// User Stories
+
+// 1. You should have a smallestCommons function that accepts an array of two numbers as an argument.
+// 2. The smallestCommons function should return the smallest common multiple that is evenly divisible by both numbers and all sequential numbers in the range between them.
+// 3. The function should handle input where the two numbers are not in numerical order.
+
+
+
+// Tests
+
+// 1. You should have a smallestCommons function.
+// 2. smallestCommons([1, 5]) should return a number.
+// 3. smallestCommons([1, 5]) should return 60.
+// 4. smallestCommons([5, 1]) should return 60.
+// 5. smallestCommons([2, 10]) should return 2520.
+// 6. smallestCommons([1, 13]) should return 360360.
+// 7. smallestCommons([23, 18]) should return 6056820.
