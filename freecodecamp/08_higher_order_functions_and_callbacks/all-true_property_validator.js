e22d5780151ceab28bead671ffdc19dfd4f9cd89
Amity <46899094+Amity-V@users.noreply.github.com>
2026-01-06 18:20:10 +0000

    add solution

--- freecodecamp/08_higher_order_functions_and_callbacks/all-true_property_validator.js
+++ freecodecamp/08_higher_order_functions_and_callbacks/all-true_property_validator.js
@@ -28,7 +28,7 @@
 // 2. The truthCheck function takes two arguments: an array of objects and a string representing a property name found in those objects.
 // 3. The function should check if the property with the name equal to the second argument has a truthy value in all the objects of the array, and return true if it has, and false otherwise.
 
-
+const truthCheck = (collection, pre) => collection.every(obj => Boolean(obj[pre]));
 
 // Tests
 
