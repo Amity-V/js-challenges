aaa153796ec483962b526fa8e3d90db5027b531f
Amity <46899094+Amity-V@users.noreply.github.com>
2026-01-20 18:41:21 +0000

    add instructions

---
+++ AdventJS/2025/08_find_the_unique_toy.js
@@ -0,0 +1,33 @@
+// Santa 🎅 wants to know what the first non-repeated letter is in a toy's name 🎁.
+// Write a function that takes a string and returns the first letter that is not repeated, ignoring uppercase and lowercase when counting, but returning the letter as it appears in the string.
+// If there is none, return an empty string ("").
+
+// Examples:
+
+// findUniqueToy('Gift') // 'G'
+// // ℹ️ The G is the first letter that is not repeated
+// // and we return it exactly as it appears
+
+// findUniqueToy('sS') // ''
+// // ℹ️ The letters are repeated, since it doesn't distinguish uppercase
+
+// findUniqueToy('reindeeR') // 'i'
+// // ℹ️ The r is repeated (even if it's uppercase)
+// // and the e as well, so the first one is 'i'
+
+// // More cases:
+// findUniqueToy('AaBbCc') // ''
+// findUniqueToy('abcDEF') // 'a'
+// findUniqueToy('aAaAaAF') // 'F'
+// findUniqueToy('sTreSS') // 'T'
+// findUniqueToy('z') // 'z'
+
+/**
+ * @param {string} toy - The toy to find the first unique one letter
+ * @returns {string} The first unique letter in the toy
+ */
+
+function findUniqueToy(toy) {
+  // Code here
+  return "";
+}
