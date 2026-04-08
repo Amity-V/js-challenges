d240d43194ff66b6ac6e98c0233e7200e14b6ad8
Amity <46899094+Amity-V@users.noreply.github.com>
2026-04-08 08:45:19 +0100

    add instructions

---
+++ AdventJS/2024/21_calculate_the_height_of_the_christmas_tree.js
@@ -0,0 +1,51 @@
+// Santa Claus 🎅 is decorating a magical Christmas tree 🪄, which this year has a special structure in the form of a binary tree. Each node of the tree represents a gift, and Santa wants to know the height of the tree to place the magical star at the top.
+
+// Your task is to write a function that calculates the height of a binary tree. The height of a binary tree is defined as the maximum number of levels from the root to a leaf. An empty tree has a height of 0.
+
+// // Tree definition
+// const tree = {
+//   value: '🎁',
+//   left: {
+//     value: '🎄',
+//     left: {
+//       value: '⭐',
+//       left: null,
+//       right: null
+//     },
+//     right: {
+//       value: '🎅',
+//       left: null,
+//       right: null
+//     }
+//   },
+//   right: {
+//     value: '❄️',
+//     left: null,
+//     right: {
+//       value: '🦌',
+//       left: null,
+//       right: null
+//     }
+//   }
+// }
+
+// // Graphical representation of the tree:
+// //        🎁
+// //       /   \
+// //     🎄     ❄️
+// //    /  \      \
+// //  ⭐   🎅      🦌
+
+// // Function call
+// treeHeight(tree)
+// // Returns: 3
+
+/**
+ * @param {{ value: string; left: any; right: any }} tree
+ * @returns {number} - Height of the tree.
+ */
+
+function treeHeight(tree) {
+  // Code here
+  return 0;
+}
