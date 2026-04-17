5fefbf896bf723c00f6ba95a7dea92f73c9a7519
Amity <46899094+Amity-V@users.noreply.github.com>
2026-04-17 08:40:24 +0100

    add instructions

---
+++ AdventJS/2024/24_check_if_trees_are_magical_mirrors.js
@@ -0,0 +1,76 @@
+// At the North Pole, the elves have two magical binary trees that generate energy 🌲🌲 to keep the Christmas star ⭐️ shining. However, for them to work properly, the trees must be in perfect sync like mirrors 🪞.
+
+// Two binary trees are mirrors if:
+// - The roots of both trees have the same value.
+// - Each node of the first tree must have its corresponding node in the opposite position in the second tree.
+
+// And the tree is represented with three properties value, left, and right. The latter two display the remaining branches (if any):
+
+// const tree = {
+//   value: '⭐️',
+//   left: {
+//     value: '🎅'
+//     // left: {...}
+//     // right: { ... }
+//   },
+//   right: {
+//     value: '🎁'
+//     // left: { ... }
+//     // right: { ...&nbsp;}
+//   }
+// }
+
+// Santa needs your help to verify if the trees are synchronized so that the star can keep shining. You must return an array where the first position indicates if the trees are synchronized and the second position returns the value of the root of the first tree.
+
+// const tree1 = {
+//   value: '🎄',
+//   left: { value: '⭐' },
+//   right: { value: '🎅' }
+// }
+
+// const tree2 = {
+//   value: '🎄',
+//   left: { value: '🎅' }
+//   right: { value: '⭐' },
+// }
+
+// isTreesSynchronized(tree1, tree2) // [true, '🎄']
+
+// /*
+//   tree1          tree2
+//    🎄              🎄
+//   / \             / \
+// ⭐   🎅         🎅   ⭐
+// */
+
+// const tree3 = {
+//   value: '🎄',
+//   left: { value: '🎅' },
+//   right: { value: '🎁' }
+// }
+
+// isTreesSynchronized(tree1, tree3) // [false, '🎄']
+
+// const tree4 = {
+//   value: '🎄',
+//   left: { value: '⭐' },
+//   right: { value: '🎅' }
+// }
+
+// isTreesSynchronized(tree1, tree4) // [false, '🎄']
+
+// isTreesSynchronized(
+//   { value: '🎅' },
+//   { value: '🧑‍🎄' }
+// ) // [false, '🎅']
+
+/**
+ * @param {object} tree1 - The first binary tree.
+ * @param {object} tree2 - The second binary tree.
+ * @returns {[boolean, string]}
+ */
+
+function isTreesSynchronized(tree1, tree2) {
+  // Code here
+  return [false, ""];
+}
